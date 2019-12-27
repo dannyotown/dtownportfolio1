c2ee35a1af81967be117fb16b00a5ec9034e2ef5
Dtown <dannyotown@gmail.com>
2019-12-27 10:08:43 -0600

    Cleaned up some extra imports

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,4 @@
 import React from "react";
-import portfoliogif from "../img/portfoliogif.gif";
 
 function AboutMe() {
   return (
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import RepoCards from "./Repos";
 import fetchGitHub from "./utils/ProjectsUtil";
 
